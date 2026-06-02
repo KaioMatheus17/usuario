8ddc699b5c7934337d6129b95e040fc96998bda4
Kaio Matheus <kaiomatheusconta@hotmail.com>
2026-06-02 12:28:08 -0300

    Adicionado class de Security

---
+++ src/main/java/com/kaio/usuario/infrastructure/repository/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.kaio.usuario.infrastructure.repository;
+
+import com.kaio.aprendendospring.infrastructure.entity.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
+}
